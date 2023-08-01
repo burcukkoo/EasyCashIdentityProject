1c6d96111c8376656a0481d8b1dd4859ad1fd869
Burcu Öztürk <110683754+burcukkoo@users.noreply.github.com>
2023-08-01 03:12:31 +0300

    ders 14 sonu

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -2,6 +2,9 @@
 using EasyCashIdentityProject.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
@@ -26,18 +29,61 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
         { 
             if(ModelState.IsValid) 
             {
-                AppUser appUser = new AppUser()
+
+                Random random = new Random();
+                int code;
+                code = random.Next(100000, 1000000);
+				string strCode = code.ToString();
+				string firstHalf = strCode.Substring(0, 3);
+				string secondHalf = strCode.Substring(3, 3);
+
+				string formattedCode = firstHalf + "-" + secondHalf;
+
+				AppUser appUser = new AppUser()
                 {
                     UserName = appUserRegisterDto.Username,
                     Name = appUserRegisterDto.Name,
                     Surname = appUserRegisterDto.Surname,
                     Email = appUserRegisterDto.Email,
+                    City = "city",
+                    District = "district",
+                    ImageUrl = "img",
+                    ConfirmCode = code,
                 };
                 var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password); 
                 //CreateAsync identity işlemini sağlar.
+
                 if (result.Succeeded) 
                 {
-                    return RedirectToAction("Index", "ConfirmMail");
+                    MimeMessage mimeMessage = new MimeMessage();
+                    MailboxAddress mailboxAddressFrom = new MailboxAddress("Easy Cash Admin", "burcuozturk.dev@gmail.com");
+                    MailboxAddress mailboxAddressTo = new MailboxAddress("User", appUser.Email);
+
+                    mimeMessage.From.Add(mailboxAddressFrom);
+                    mimeMessage.To.Add(mailboxAddressTo);
+
+                    var bodyBuilder = new BodyBuilder();
+                    bodyBuilder.TextBody = "Your code to complete the registration process is: " + formattedCode;
+                    mimeMessage.Body = bodyBuilder.ToMessageBody();
+                    mimeMessage.Subject = "Easy Cash Confirmation Code";
+
+					using (var client = new SmtpClient())
+					{
+						await client.ConnectAsync("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+						await client.AuthenticateAsync("burcuozturk.dev@gmail.com", "ddirbzgdyxbkmqlf\r\n");
+						await client.SendAsync(mimeMessage);
+						await client.DisconnectAsync(true);
+					}
+
+
+					return RedirectToAction("Index", "ConfirmMail");
+                }
+                else
+                {
+                    foreach(var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
                 }
             }
             return View();
